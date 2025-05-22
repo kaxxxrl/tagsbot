91e23b5aa246b56eff48d09c9786eebc1279a1ca
kaxxxrl <totalrthroandemomusics@gmail.com>
2025-05-22 17:12:08 +0200

    Update pozegnania.js

--- pozegnania.js
+++ pozegnania.js
@@ -8,7 +8,8 @@ async function sendFarewell(member) {
   const embed = new EmbedBuilder()
     .setColor('#FF0000')
     .setTitle('Do zobaczenia! <a:3124_cry:1375120201909010472>')
-    .setDescription(`> <a:73288animatedarrowred:1375119792029306930> **${member.user.tag}** opuścił nasz serwer. Może kiedyś wrócisz ?!`)
+    .setDescription(`> <a:73288animatedarrowred:1375119792029306930> **${member.user.tag}** opuścił nasz serwer. Może kiedyś wrócisz?!`)
+    .setThumbnail(member.user.displayAvatarURL({ dynamic: true })) // 👈 tu dodane
     .setTimestamp();
 
   await channel.send({ embeds: [embed] });
