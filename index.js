ff49b34516ce0fae5f91318b8a2dcdd971f919c2
kaxxxrl <totalrthroandemomusics@gmail.com>
2025-05-22 17:21:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { Client, GatewayIntentBits } = require('discord.js');
 require('dotenv').config();
 
 const powitania = require('./powitania');
-const pozegnania = require('./pozegnania'); // bez "ż"
+const pozegnania = require('./pozegnania');
 
 const client = new Client({
     intents: [
@@ -10,18 +10,6 @@ const client = new Client({
         GatewayIntentBits.GuildMembers,
         GatewayIntentBits.GuildMessages
     ]
-});
-
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => {
-  res.send('Bot działa 🚀');
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`🌐 Serwer Express działa na porcie ${PORT}`);
 });
 
 client.once('ready', () => {
@@ -33,7 +21,7 @@ client.on('guildMemberAdd', member => {
 });
 
 client.on('guildMemberRemove', member => {
-    pozegnania(member); // ✅ poprawna wersja
+    pozegnania(member);
 });
 
 client.login(process.env.TOKEN);
