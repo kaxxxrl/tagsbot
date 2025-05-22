bd2021708933e37a7b8a6d5232626e24d07e731e
kaxxxrl <totalrthroandemomusics@gmail.com>
2025-05-22 17:21:27 +0200

    Update pozegnania.js

--- pozegnania.js
+++ pozegnania.js
@@ -1,18 +1,16 @@
 const { EmbedBuilder } = require('discord.js');
+const config = require('./config.json');
 
-async function sendFarewell(member) {
-  const channelId = '1375114457239388160'; 
-  const channel = member.guild.channels.cache.get(channelId);
-  if (!channel) return;
+module.exports = async function pozegnanie(member) {
+    const channel = member.guild.channels.cache.get(config.farewellChannelId);
+    if (!channel) return;
 
-  const embed = new EmbedBuilder()
-    .setColor('#FF0000')
-    .setTitle('Do zobaczenia! <a:3124_cry:1375120201909010472>')
-    .setDescription(`> <a:73288animatedarrowred:1375119792029306930> **${member.user.tag}** opuścił nasz serwer. Może kiedyś wrócisz?!`)
-    .setThumbnail(member.user.displayAvatarURL({ dynamic: true })) // 👈 tu dodane
-    .setTimestamp();
+    const embed = new EmbedBuilder()
+        .setColor('#FF0000')
+        .setTitle('Do zobaczenia! <a:3124_cry:1375120201909010472>')
+        .setDescription(`> <a:73288animatedarrowred:1375119792029306930> **${member.user.tag}** opuścił serwer.`)
+        .setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
+        .setTimestamp();
 
-  await channel.send({ embeds: [embed] });
-}
-
-module.exports = sendFarewell;
+    await channel.send({ embeds: [embed] });
+};
