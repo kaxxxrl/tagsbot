4d14ab79c46d8dc7b100bfd0747aae0780210cc2
kaxxxrl <totalrthroandemomusics@gmail.com>
2025-05-22 16:51:56 +0200

    Create pożegnania.js

---
+++ pożegnania.js
@@ -0,0 +1,17 @@
+const { EmbedBuilder } = require('discord.js');
+
+async function sendFarewell(member) {
+  const channelId = '1375114457239388160'; 
+  const channel = member.guild.channels.cache.get(channelId);
+  if (!channel) return;
+
+  const embed = new EmbedBuilder()
+    .setColor('#FF0000')
+    .setTitle('Do zobaczenia! <a:3124_cry:1375120201909010472>')
+    .setDescription(`> <a:73288animatedarrowred:1375119792029306930> **${member.user.tag}** opuścił nasz serwer. Może kiedyś wrócisz ?!`)
+    .setTimestamp();
+
+  await channel.send({ embeds: [embed] });
+}
+
+module.exports = { sendFarewell };
