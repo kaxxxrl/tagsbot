d29b294374c3d0773223e8063dc7866b0a921b61
kaxxxrl <totalrthroandemomusics@gmail.com>
2025-05-22 16:47:06 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+
+const { Client, GatewayIntentBits } = require('discord.js');
+require('dotenv').config();
+const powitania = require('./powitania');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildMessages
+    ]
+});
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Bot działa 🚀');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`🌐 Serwer Express działa na porcie ${PORT}`);
+});
+
+client.once('ready', () => {
+    console.log(`✅ Zalogowano jako ${client.user.tag}`);
+});
+
+client.on('guildMemberAdd', member => {
+    powitania(member);
+});
+
+client.login(process.env.TOKEN);
